995ac670b1e5c8705bda7fce571eb8bc3ebc16ea
Socially Awkward Streams <candice.radam@gmail.com>
2023-02-04 20:16:29 -0600

    Starting off slow and trying to configure webpack - added script src path to html.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = () => {
       install: './src/js/install.js'
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: 'main.bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
