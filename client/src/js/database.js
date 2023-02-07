43c1a67880ed96d0bfa46a4373109a1e6f32208d
Socially Awkward Streams <candice.radam@gmail.com>
2023-02-07 09:18:56 -0600

    Get ALL from database.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,17 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+console.error('putDb not implemented');
 console.log('PUT to the database.');
   const jateDb = await openDB('jate', 1); // Create a connection to the database database and version we want to use.
   const tx = jateDb.transaction('jate', 'readwrite'); // Create a new transaction and specify the database and data privileges.
   const store = tx.objectStore('jate'); // Open up the desired object store.
-  const request = store.put({ id: 1, jateDb: content });  //***Is this correct? 
+  const request = store.put({ id: 1, value: content });  //***Is this correct? 
   const result = await request;
   console.log('🚀 Success! Data saved to the database', result);
+}
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly'); //***Readonly, or Readwrite?/
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 initdb();
