881999d4c89bacf5a838b24940c154582b702b55
Socially Awkward Streams <candice.radam@gmail.com>
2023-02-07 08:56:13 -0600

    Working on the database connection in databse.js.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,6 +14,14 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
+console.log('Posted to the database.');
+
+// Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+// Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
