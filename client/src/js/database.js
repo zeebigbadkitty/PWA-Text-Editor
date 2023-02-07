45f23c597e4015364bd693cd17c66a9ed2d1e60e
Socially Awkward Streams <candice.radam@gmail.com>
2023-02-07 09:08:02 -0600

    Working on put request.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,13 +14,13 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
-console.log('Posted to the database.');
-
-// Create a connection to the database database and version we want to use.
-  const jateDb = await openDB('jate', 1);
-// Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readwrite');
-
+console.log('PUT to the database.');
+  const jateDb = await openDB('jate', 1); // Create a connection to the database database and version we want to use.
+  const tx = jateDb.transaction('jate', 'readwrite'); // Create a new transaction and specify the database and data privileges.
+  const store = tx.objectStore('jate'); // Open up the desired object store.
+  const request = store.put({ id: 1, jateDb: content });  //***Is this correct? 
+  const result = await request;
+  console.log('🚀 Success! Data saved to the database', result);
 
 
 // TODO: Add logic for a method that gets all the content from the database
